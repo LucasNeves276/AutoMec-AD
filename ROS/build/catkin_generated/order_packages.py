21011d384d6517ed47f2a8498015abebefa14e27
manuelgitgomes <gomesnelito@gmail.com>
2020-12-18 14:31:43 +0000

    Ros Package first commit

---
+++ ROS/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nel/PycharmProjects/AutoMec-AD/ROS/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nel/PycharmProjects/AutoMec-AD/ROS/devel;/opt/ros/noetic'.split(';') if '/home/nel/PycharmProjects/AutoMec-AD/ROS/devel;/opt/ros/noetic' != '' else []
